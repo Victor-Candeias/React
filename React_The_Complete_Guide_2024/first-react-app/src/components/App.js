da061b7eb31d3a0797179ff5365e4bcf48b274e3
Victor Candeias <victor.s.candeias@ctt.pt>
2024-08-05 17:55:13 +0100

    [VC] - First React App
    First React App

---
+++ React_The_Complete_Guide_2024/first-react-app/src/components/App.js
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import "./App.css";
+
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem"
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect())",
+    "Dynamic rendering"
+  ],
+  [
+    "Official web page (react.dev)",
+    "Next.js (Fullstack framework)",
+    "React Native (build native mobile apps with React)"
+  ],
+  [
+    "Vanilla JavaScript requires imperative programming",
+    "Imperative Programming: You define all the steps needed to achieve a result",
+    "React on the other hand embraces declarative programming",
+    "With React, you define the goal and React figures out how to get there"
+  ],
+  [
+    "jdsfhkshjhfs kjfh ksh fkjdhf kdhs",
+    "jhb ksldhkf kjgf kjskjhs fkjsfjh sfk",
+    " sdfkjgkjfhkjh kjfh kjh fkjh kjdg",
+    "uw ykhkjdhgkjdfhg kjdhkjd he"
+  ]
+];
+
+export default function App() {
+  const [activeContentIndex, setActiveContentIndex] = useState(0);
+
+  return (
+    <div>
+      <header>
+        <img src={process.env.PUBLIC_URL + '/logo192.png'} alt="React logo" />
+        <div>
+          <h1>React.js</h1>
+          <p>i.e., using the React library for rendering the UI</p>
+        </div>
+      </header>
+
+      <div id="tabs">
+        <menu>
+          <button
+            className={activeContentIndex === 0 ? "active" : ""}
+            onClick={() => setActiveContentIndex(0)}
+          >
+            Why React?
+          </button>
+          <button
+            className={activeContentIndex === 1 ? "active" : ""}
+            onClick={() => setActiveContentIndex(1)}
+          >
+            Core Features
+          </button>
+          <button
+            className={activeContentIndex === 2 ? "active" : ""}
+            onClick={() => setActiveContentIndex(2)}
+          >
+            Related Resources
+          </button>
+          <button
+            className={activeContentIndex === 3 ? "active" : ""}
+            onClick={() => setActiveContentIndex(3)}
+          >
+            Other Resources
+          </button>
+          <button
+            className={activeContentIndex === 4? "active" : ""}
+            onClick={() => setActiveContentIndex(4)}
+          >
+            asd dasdda dsd
+          </button>
+        </menu>
+        <div id="tab-content">
+          <ul>
+            {content[activeContentIndex].map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
